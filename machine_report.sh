4fd31b204af6f12766647241f3ac43a88a4f7e77
DanielTolentino <danieltolentino@protonmail.com>
2024-06-16 12:43:30 -0300

    chore: Update machine_report.sh to improve bar graph formatting and disk information

--- machine_report.sh
+++ machine_report.sh
@@ -2,29 +2,33 @@
 # TR-100 Machine Report
 # Copyright © 2024, U.S. Graphics, LLC. BSD-3-Clause License.
 
+export LC_NUMERIC=C
+
 # Basic configuration, change as needed
 report_title="UNITED STATES GRAPHICS COMPANY"
 #zfs_filesystem="zroot/ROOT/os"
 
 # Utilities
 bar_graph() {
-	local percent
-	local num_blocks
-	local width=29
-	local graph=""
-	local used=$1
-	local total=$2
+    local percent
+    local num_blocks
+    local width=29
+    local graph=""
+    local used=$1
+    local total=$2
 
-	percent=$(printf "%.2f" "$(echo "$used / $total * 100" | bc -l)")
-	num_blocks=$(echo "scale=2; ${percent}/100*${width}" | bc -l | numfmt --from=iec --format %.0f)
-	for ((i = 0; i < num_blocks; i++)); do
-		graph+="█"
-	done
-	for ((i = 0; i < width - num_blocks; i++)); do
-		graph+="░"
-	done
-	printf "%s" "${graph}"
+    percent=$(echo "scale=2; $used / $total * 100" | bc)
+    num_blocks=$(printf "%.0f" $(echo "scale=2; ($percent / 100) * $width" | bc))
+    
+    for ((i = 0; i < num_blocks; i++)); do
+        graph+="█"
+    done
+    for ((i = num_blocks; i < width; i++)); do
+        graph+="░"
+    done
+    printf "%s" "${graph}"
 }
+
 
 # Operating System Information
 source /etc/os-release
@@ -40,7 +44,7 @@ os_kernel=$({
 net_current_user=$(whoami)
 net_hostname=$(hostname -f)
 net_machine_ip=$(hostname -I)
-net_client_ip=$(who am i --ips | awk '{print $5}')
+#net_client_ip=$(who am i --ips | awk '{print $5}')
 net_dns_ips=()
 while read -r line; do
 	ip=$(echo "$line" | awk '{print $2}')
@@ -51,7 +55,6 @@ done < <(grep 'nameserver' /etc/resolv.conf)
 cpu_model="$(lscpu | grep 'Nome do modelo' | grep -v 'BIOS' | cut -f 2 -d ':' | awk '{print $1 " "  $2 " " $3}')"
 cpu_hypervisor="$(lscpu | grep 'Hypervisor vendor' | cut -f 2 -d ':' | awk '{$1=$1}1')"
 cpu_cores="$(nproc --all)"
-df -h / | awk 'NR==2 {print $2, $3, $4, $5, $6}'
 cpu_cores_per_socket="$(lscpu | grep 'Núcleo(s) por soquete' | cut -f 2 -d ':' | awk '{$1=$1}1')"
 cpu_sockets="$(lscpu | grep 'Soquete(s)' | cut -f 2 -d ':' | awk '{$1=$1}1')"
 cpu_freq="$(grep 'cpu MHz' /proc/cpuinfo | cut -f 2 -d ':' | awk 'NR==1' | awk '{$1=$1/1000}1' | numfmt --format="%.2f")"
@@ -61,7 +64,7 @@ load_avg_5min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f2 | 
 load_avg_15min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f3 | tr -d ' ')
 
 cpu_1min_bar_graph=$(bar_graph "$load_avg_1min" "$cpu_cores")
-cpu_5min_bar_graph=$(bar_graph "$load_avg_5min" "$cpu_cores")
+#cpu_5min_bar_graph=$(bar_graph "$load_avg_5min" "$cpu_cores")
 cpu_15min_bar_graph=$(bar_graph "$load_avg_15min" "$cpu_cores")
 
 # Memory Information
@@ -69,24 +72,18 @@ mem_total=$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')
 mem_available=$(grep 'MemAvailable' /proc/meminfo | awk '{print $2}')
 mem_used=$((mem_total - mem_available))
 mem_percent=$(echo "$mem_used / $mem_total * 100" | bc -l)
-mem_percent=$(printf "%.2f" "$mem_percent")
+mem_percent=$(printf "%.0f" "$mem_percent")
 mem_total_gb=$(echo "$mem_total" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f)
-mem_available_gb=$(echo "$mem_available" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f) # Not used currently
 mem_used_gb=$(echo "$mem_used" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f)
 mem_bar_graph=$(bar_graph "$mem_used" "$mem_total")
 
 # Disk Information
-
-disk_used="$(
-	df -h / | awk 'NR==2 {print $2, $3, $4, $5 }'
-)"
-disk_availabe="$(
-	df -h / | awk 'NR==2 {print $4 }'
-)"
-disk_usage="$(
-	df -h / | awk 'NR==2 {print $3 }'
-)"
-disk_bar_graph=$(bar_graph "$disk_used" "$disk_available")
+disk_info=($(df -h / | awk 'NR==2 {print $2, $3, $4, $5}'))
+disk_total="${disk_info[0]}"
+disk_used="${disk_info[1]}"
+disk_available="${disk_info[2]}"
+disk_usage="${disk_info[3]}"
+disk_bar_graph=$(bar_graph "$disk_used" "$disk_total")
 
 # Last login and Uptime
 last_login=$(lastlog -u root)
@@ -126,12 +123,10 @@ printf "│ %-10s │ %-29s │\n" "CORES" "$cpu_cores_per_socket vCPU(s) / $cpu
 printf "│ %-10s │ %-29s │\n" "HYPERVISOR" "$cpu_hypervisor"
 printf "│ %-10s │ %-29s │\n" "CPU FREQ" "$cpu_freq GHz"
 printf "│ %-10s │ %-29s │\n" "LOAD  1m" "$cpu_1min_bar_graph"
-printf "│ %-10s │ %-29s │\n" "LOAD  5m" "$cpu_5min_bar_graph"
 printf "│ %-10s │ %-29s │\n" "LOAD 15m" "$cpu_15min_bar_graph"
 printf "├────────────┼───────────────────────────────┤\n"
-printf "│ %-10s │ %-29s │\n" "VOLUME" "$disk_used"
+printf "│ %-10s │ %-29s │\n" "VOLUME" "$disk_total $disk_used $disk_available [$disk_usage]"
 printf "│ %-10s │ %-29s │\n" "DISK USAGE" "$disk_bar_graph"
-printf "│ %-10s │ %-29s │\n" "ZFS HEALTH" "$zfs_health"
 printf "├────────────┼───────────────────────────────┤\n"
 printf "│ %-10s │ %-29s │\n" "MEMORY" "${mem_used_gb}/${mem_total_gb} GiB [${mem_percent}%]"
 printf "│ %-10s │ %-29s │\n" "USAGE" "${mem_bar_graph}"
