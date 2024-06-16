07d3ad2e79a2a24b4a3c25a0663f214d05ed7892
DanielTolentino <danieltolentino@protonmail.com>
2024-06-16 12:17:19 -0300

    Adjust for Ubuntu

--- machine_report.sh
+++ machine_report.sh
@@ -4,32 +4,37 @@
 
 # Basic configuration, change as needed
 report_title="UNITED STATES GRAPHICS COMPANY"
-zfs_filesystem="zroot/ROOT/os"
+#zfs_filesystem="zroot/ROOT/os"
 
 # Utilities
 bar_graph() {
-    local percent
-    local num_blocks
-    local width=29
-    local graph=""
-    local used=$1
-    local total=$2
+	local percent
+	local num_blocks
+	local width=29
+	local graph=""
+	local used=$1
+	local total=$2
 
-    percent=$(printf "%.2f" "$(echo "$used / $total * 100" | bc -l)")
-    num_blocks=$(echo "scale=2; ${percent}/100*${width}" | bc -l | numfmt --from=iec --format %.0f)
-    for (( i = 0; i < num_blocks; i++ )); do
-        graph+="█"
-    done
-    for (( i=0; i < width - num_blocks; i++ )); do
-        graph+="░"
-    done
-    printf "%s" "${graph}"
+	percent=$(printf "%.2f" "$(echo "$used / $total * 100" | bc -l)")
+	num_blocks=$(echo "scale=2; ${percent}/100*${width}" | bc -l | numfmt --from=iec --format %.0f)
+	for ((i = 0; i < num_blocks; i++)); do
+		graph+="█"
+	done
+	for ((i = 0; i < width - num_blocks; i++)); do
+		graph+="░"
+	done
+	printf "%s" "${graph}"
 }
 
 # Operating System Information
 source /etc/os-release
-os_name="${ID^} $(cat /etc/debian_version) ${VERSION_CODENAME^}"
-os_kernel=$({ uname; uname -r; } | tr '\n' ' ')
+os_name="${ID^} ${VERSION^}"
+
+#os_name="${ID^} $(cat /etc/lsb-release) ${DISTRIB_CODENAME^}"
+os_kernel=$({
+	uname
+	uname -r
+} | tr '\n' ' ')
 
 # Network Information
 net_current_user=$(whoami)
@@ -38,21 +43,22 @@ net_machine_ip=$(hostname -I)
 net_client_ip=$(who am i --ips | awk '{print $5}')
 net_dns_ips=()
 while read -r line; do
-    ip=$(echo "$line" | awk '{print $2}')
-    net_dns_ips+=("$ip")
+	ip=$(echo "$line" | awk '{print $2}')
+	net_dns_ips+=("$ip")
 done < <(grep 'nameserver' /etc/resolv.conf)
 
 # CPU Information
-cpu_model="$(lscpu | grep 'Model name' | grep -v 'BIOS' | cut -f 2 -d ':' | awk '{print $1 " "  $2 " " $3}')"
+cpu_model="$(lscpu | grep 'Nome do modelo' | grep -v 'BIOS' | cut -f 2 -d ':' | awk '{print $1 " "  $2 " " $3}')"
 cpu_hypervisor="$(lscpu | grep 'Hypervisor vendor' | cut -f 2 -d ':' | awk '{$1=$1}1')"
 cpu_cores="$(nproc --all)"
-cpu_cores_per_socket="$(lscpu | grep 'Core(s) per socket' | cut -f 2 -d ':'| awk '{$1=$1}1')"
-cpu_sockets="$(lscpu | grep 'Socket(s)' | cut -f 2 -d ':' | awk '{$1=$1}1')"
-cpu_freq="$(grep 'cpu MHz' /proc/cpuinfo | cut -f 2 -d ':' | awk 'NR==1' | awk '{$1=$1}1' | numfmt --from-unit=M --to-unit=G --format %.2f)"
+df -h / | awk 'NR==2 {print $2, $3, $4, $5, $6}'
+cpu_cores_per_socket="$(lscpu | grep 'Núcleo(s) por soquete' | cut -f 2 -d ':' | awk '{$1=$1}1')"
+cpu_sockets="$(lscpu | grep 'Soquete(s)' | cut -f 2 -d ':' | awk '{$1=$1}1')"
+cpu_freq="$(grep 'cpu MHz' /proc/cpuinfo | cut -f 2 -d ':' | awk 'NR==1' | awk '{$1=$1/1000}1' | numfmt --format="%.2f")"
 
 load_avg_1min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f1 | tr -d ' ')
 load_avg_5min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f2 | tr -d ' ')
-load_avg_15min=$(uptime| awk -F'load average: ' '{print $2}' | cut -d ',' -f3 | tr -d ' ')
+load_avg_15min=$(uptime | awk -F'load average: ' '{print $2}' | cut -d ',' -f3 | tr -d ' ')
 
 cpu_1min_bar_graph=$(bar_graph "$load_avg_1min" "$cpu_cores")
 cpu_5min_bar_graph=$(bar_graph "$load_avg_5min" "$cpu_cores")
@@ -66,19 +72,21 @@ mem_percent=$(echo "$mem_used / $mem_total * 100" | bc -l)
 mem_percent=$(printf "%.2f" "$mem_percent")
 mem_total_gb=$(echo "$mem_total" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f)
 mem_available_gb=$(echo "$mem_available" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f) # Not used currently
-mem_used_gb=$(echo "$mem_used" | numfmt  --from-unit=Ki --to-unit=Gi --format %.2f)
+mem_used_gb=$(echo "$mem_used" | numfmt --from-unit=Ki --to-unit=Gi --format %.2f)
 mem_bar_graph=$(bar_graph "$mem_used" "$mem_total")
 
 # Disk Information
-# TODO: Add checks if zfs file system exists
-# WARNING: This script assumes that the zfs file system is located at $zfs_filesystem
-zfs_health=$(zpool status -x zroot | grep -q "is healthy" && echo  "HEALTH O.K.")
-zfs_available=$( zfs get -o value -Hp available "$zfs_filesystem")
-zfs_used=$( zfs get -o value -Hp used "$zfs_filesystem")
-zfs_available_gb=$(echo "$zfs_available" | numfmt --to-unit=G --format %.2f)
-zfs_used_gb=$(echo "$zfs_used" | numfmt --to-unit=G --format %.2f)
-disk_percent=$(printf "%.2f" "$(echo "$zfs_used / $zfs_available * 100" | bc -l)")
-disk_bar_graph=$(bar_graph "$zfs_used" "$zfs_available")
+
+disk_used="$(
+	df -h / | awk 'NR==2 {print $2, $3, $4, $5 }'
+)"
+disk_availabe="$(
+	df -h / | awk 'NR==2 {print $4 }'
+)"
+disk_usage="$(
+	df -h / | awk 'NR==2 {print $3 }'
+)"
+disk_bar_graph=$(bar_graph "$disk_used" "$disk_available")
 
 # Last login and Uptime
 last_login=$(lastlog -u root)
@@ -101,15 +109,15 @@ printf "│ %-10s │ %-29s │\n" "MACHINE IP" "$net_machine_ip"
 printf "│ %-10s │ %-29s │\n" "CLIENT  IP" "$net_client_ip"
 dns_ip_count=${#net_dns_ips[@]}
 if [ "$dns_ip_count" -eq 1 ]; then
-    printf "│ %-10s │ %-29s │\n" "DNS     IP" "${net_dns_ips[0]}"
+	printf "│ %-10s │ %-29s │\n" "DNS     IP" "${net_dns_ips[0]}"
 else
-    for ((i=0; i<$dns_ip_count; i++)); do
-        if [ "$i" -eq 0 ]; then
-            printf "│ %-10s │ %-29s │\n" "DNS IP/s 1" "${net_dns_ips[$i]}"
-        else
-            printf "│ %-10s │ %-29s │\n" "         $((i+1))" "${net_dns_ips[$i]}"
-        fi
-    done
+	for ((i = 0; i < $dns_ip_count; i++)); do
+		if [ "$i" -eq 0 ]; then
+			printf "│ %-10s │ %-29s │\n" "DNS IP/s 1" "${net_dns_ips[$i]}"
+		else
+			printf "│ %-10s │ %-29s │\n" "         $((i + 1))" "${net_dns_ips[$i]}"
+		fi
+	done
 fi
 printf "│ %-10s │ %-29s │\n" "USER" "$net_current_user"
 printf "├────────────┼───────────────────────────────┤\n"
@@ -121,7 +129,7 @@ printf "│ %-10s │ %-29s │\n" "LOAD  1m" "$cpu_1min_bar_graph"
 printf "│ %-10s │ %-29s │\n" "LOAD  5m" "$cpu_5min_bar_graph"
 printf "│ %-10s │ %-29s │\n" "LOAD 15m" "$cpu_15min_bar_graph"
 printf "├────────────┼───────────────────────────────┤\n"
-printf "│ %-10s │ %-29s │\n" "VOLUME" "$zfs_used_gb/$zfs_available_gb GB [$disk_percent%]"
+printf "│ %-10s │ %-29s │\n" "VOLUME" "$disk_used"
 printf "│ %-10s │ %-29s │\n" "DISK USAGE" "$disk_bar_graph"
 printf "│ %-10s │ %-29s │\n" "ZFS HEALTH" "$zfs_health"
 printf "├────────────┼───────────────────────────────┤\n"
